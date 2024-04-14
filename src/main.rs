c8eaa7bd44a9e5aba3e3f2b4f8984f527d17d89d
Nabin Khanal <nabinkhanal@skiff.com>
2024-04-15 01:09:28 +0545

    PING PONG

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,21 @@
 // Uncomment this block to pass the first stage
-use std::net::TcpListener;
+use std::io::{Read, Write};
+use std::net::{TcpListener, TcpStream};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
-            }
+            Ok(stream) => handle_stream(stream),
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
+}
+
+fn handle_stream(mut stream: TcpStream) {
+    let mut buf = [0; 1024];
+    let _ = stream.read(&mut buf);
+    let _ = stream.write_all(String::from("+PONG\r\n").as_bytes());
 }
