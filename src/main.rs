228f4fcdb8b8a3dafacc9b3904a332ac58eb35d2
Nabin Khanal <nabinkhanal@skiff.com>
2024-04-15 22:43:42 +0545

    handle multiple response from single connection

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
 // Uncomment this block to pass the first stage
 use std::io::{Read, Write};
 use std::net::{TcpListener, TcpStream};
+use std::thread;
 
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+    let listener = TcpListener::bind("127.0.0.1:6379")
+        .expect("Failed to bind listener to the provided address.");
     for stream in listener.incoming() {
         match stream {
-            Ok(stream) => handle_stream(stream),
+            Ok(stream) => {
+                thread::spawn(move || {
+                    handle_stream(stream);
+                });
+            }
             Err(e) => {
                 println!("error: {}", e);
             }
@@ -16,6 +22,8 @@ fn main() {
 
 fn handle_stream(mut stream: TcpStream) {
     let mut buf = [0; 1024];
-    let _ = stream.read(&mut buf);
-    let _ = stream.write_all(String::from("+PONG\r\n").as_bytes());
+    loop {
+        let _ = stream.read(&mut buf);
+        let _ = stream.write_all(String::from("+PONG\r\n").as_bytes());
+    }
 }
