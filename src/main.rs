993b15ae539080edf9b0b88d41c1fe0077851b11
Nabin Khanal <nabinkhanal@skiff.com>
2024-04-15 23:31:24 +0545

    bruteforce decoding

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ fn main() {
             Ok(stream) => {
                 // handle each stream in a different thread
                 thread::spawn(move || {
-                    handle_client(stream);
+                    let _ = handle_client(stream);
                 });
             }
             Err(e) => {
@@ -21,10 +21,25 @@ fn main() {
     }
 }
 
-fn handle_client(mut stream: TcpStream) {
+fn handle_client(mut stream: TcpStream) -> Result<(), Box<dyn std::error::Error>> {
     let mut buf = [0; 1024];
     loop {
-        let _ = stream.read(&mut buf);
-        let _ = stream.write_all(String::from("+PONG\r\n").as_bytes());
+        let n = stream.read(&mut buf)?;
+        let request = String::from_utf8(buf[..n].to_vec())?;
+        let request = request.trim();
+        let mut request = request.split_whitespace();
+
+        let command = request.next().ok_or("Command is not provided.")?;
+        match command {
+            "ping" => {
+                let _ = stream.write_all("+PONG\r\n".as_bytes());
+            }
+            "echo" => {
+                let message = request.next().unwrap_or("");
+                let message = format!("${}{}\r\n", message.len(), message);
+                let _ = stream.write_all(message.as_bytes());
+            }
+            _ => {}
+        }
     }
 }
